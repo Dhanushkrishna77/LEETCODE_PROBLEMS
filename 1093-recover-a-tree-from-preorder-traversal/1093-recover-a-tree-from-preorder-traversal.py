bdccda4c3db92d7775443140781197f12d703449
Dhanushkrishna77 <166887492+Dhanushkrishna77@users.noreply.github.com>
2025-02-22 21:52:57 +0530

    Time: 38 ms (64.29%), Space: 13.5 MB (21.43%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.py
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def recoverFromPreorder(self, traversal):
-
-        # helper function for tree building
-        # curr_depth stores the depth of current subtree root
-        # [start, end] stores the start and end indices of current subtree segment
         def treebuilder(curr_depth, start, end): 
             if start > end:
                 return None
@@ -11,23 +7,21 @@ class Solution(object):
             if start == end:
                 return TreeNode(values[start])
             
-            root = TreeNode(values[start]) # start index is the root
+            root = TreeNode(values[start])
 
-            # try to find the right subtree root's index
             right_tree_index = -1 
             for i in range(start + 2, end + 1):
                 if depths[i] == curr_depth + 1:
                     right_tree_index = i
 
-            if right_tree_index != -1: # right tree exists
+            if right_tree_index != -1: 
                 root.left = treebuilder(curr_depth + 1, start + 1, right_tree_index - 1)
                 root.right = treebuilder(curr_depth + 1, right_tree_index, end)
-            else: # right tree does not exist
+            else: 
                 root.left = treebuilder(curr_depth + 1, start + 1, end)
 
             return root
 
-        # Pre-prossessing: Obtain 2 arrays of values and depths
         values = []
         depths = [0]
 
@@ -36,18 +30,17 @@ class Solution(object):
         i = 0
 
         while i < len(traversal):
-            if traversal[i].isnumeric(): # obtain the value
+            if traversal[i].isnumeric(): 
                 while i < len(traversal) and traversal[i].isnumeric():
                     curr_num += traversal[i]
                     i += 1
                 values.append(int(curr_num))
                 curr_num = ""
-            else: # obtain the depth of that value by counting dashes
+            else: 
                 while i < len(traversal) and not traversal[i].isnumeric():
                     depth += 1
                     i += 1
                 depths.append(depth)
                 depth = 0
 
-        # build tree and return root
         return treebuilder(0, 0, len(values) - 1)
