2bd2e8a1f85274ef22b37e1709fc9b4de98f6f9d
Dhanushkrishna77 <166887492+Dhanushkrishna77@users.noreply.github.com>
2025-03-09 10:40:50 +0530

    Time: 4 ms (68.16%), Space: 63.1 MB (60.06%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+       
+        int L = 0;
+        int R = k-1;
+        int count = 0;
+        int mostRecentDisruptionIdx = -1;
+        for(int i = 1; i <= R; i++){
+            if(colors[i] == colors[i-1])
+                mostRecentDisruptionIdx = i-1;
+        }
+        while(R < colors.length + k-1){
+            if(mostRecentDisruptionIdx < L)
+                count++;
+            R++;
+            L++;
+            if(R < colors.length + k -1)
+                if(colors[R%colors.length] == colors[(R-1)%colors.length])
+                    mostRecentDisruptionIdx = R-1;
+        }
+        return count;
+    }
+}
