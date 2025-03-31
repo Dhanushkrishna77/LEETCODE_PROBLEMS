991ae24f4965228385c3f52847df84ab44bd9fd5
Dhanushkrishna77 <166887492+Dhanushkrishna77@users.noreply.github.com>
2025-03-31 23:08:39 +0530

    Time: 551 ms (5.37%), Space: 39.8 MB (5.79%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.py
@@ -1,4 +1,8 @@
+from typing import List
+from itertools import pairwise
+from heapq import nlargest, nsmallest
+
 class Solution:
     def putMarbles(self, A: List[int], k: int) -> int:
-        B = [a + b for a,b in pairwise(A)]
+        B = [a + b for a, b in pairwise(A)]
         return sum(nlargest(k - 1, B)) - sum(nsmallest(k - 1, B))
