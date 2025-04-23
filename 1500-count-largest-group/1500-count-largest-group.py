7d7384b9d682289845e7858e14a9154fc3151859
Dhanushkrishna77 <166887492+Dhanushkrishna77@users.noreply.github.com>
2025-04-23 05:43:28 +0530

    Time: 155 ms (11.84%), Space: 13 MB (27.63%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countLargestGroup(self, n):
+        res = []
+        for i in range(1, n+1):
+            res.append(sum(int(x) for x in str(i)))      
+        c = collections.Counter(res)
+        x = [i for i in c.values() if i == max(c.values())]
+        return len(x)
+    
+    
