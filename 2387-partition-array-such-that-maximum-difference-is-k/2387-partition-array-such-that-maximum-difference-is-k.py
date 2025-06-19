16bfea49af3aa673100208434e4a0356e5f86cda
Dhanushkrishna77 <166887492+Dhanushkrishna77@users.noreply.github.com>
2025-06-19 22:02:58 +0530

    Time: 175 ms (15.79%), Space: 21.7 MB (40.35%) - LeetHub

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def partitionArray(self, nums, k):
+        nums.sort()
+        c=0
+        i=0
+        while i<len(nums):
+            c+=1 
+            start=nums[i]
+            while i<len(nums)and nums[i]-start<=k:
+                i+=1
+        return c
