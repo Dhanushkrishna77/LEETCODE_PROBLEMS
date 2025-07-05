d38d80d71fc6740fda8f7b4789b1c3798e4bc982
Dhanushkrishna77 <166887492+Dhanushkrishna77@users.noreply.github.com>
2025-07-05 09:26:09 +0530

    Time: 0 ms (100%), Space: 12.3 MB (79.92%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkValidString(self, s):
+        while s != s.replace("()", ""):
+            s = s.replace("()", "")
+        queue = []
+        for i in range(len(s)):
+            if s[i] in ["(", "*"]:
+                queue.append(1)
+            else:
+                if queue:
+                    queue.pop()
+                else:
+                    return False
+        queue = []
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] in [")", "*"]:
+                queue.append(1)
+            else:
+                if queue:
+                    queue.pop()
+                else:
+                    return False
+        return True
